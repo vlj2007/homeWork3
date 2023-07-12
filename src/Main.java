a038f97cb7a26632a82f6446a977ef8a85467115
vlj2007 <vlj2007@yandex.ru>
2023-07-12 17:47:05 +0700

    I_created_variables_and_displayed_their_content_in_task1

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,19 @@ public class Main {
 
     public static void task1(){
         System.out.println("Задача 1");
+        int peopleInHere = 30;
+        byte inPut = 100;
+        short  stepCounter = 458;
+        long speedLight = 299792458;
+        float saltBag = 7.6F;
+        double moneyInBudget = 1000000678543.34;
+
+        System.out.println("Значение переменной peopleInHere с типом int равно " + peopleInHere);
+        System.out.println("Значение переменной inPut с типом byte равно " + inPut);
+        System.out.println("Значение переменной stepCounter с типом short равно " + stepCounter);
+        System.out.println("Значение переменной speedLight с типом long равно "+ speedLight);
+        System.out.println("Значение переменной saltBag с типом float равно " + saltBag);
+        System.out.println("Значение переменной moneyInBudget с типом double равно " + moneyInBudget);
 
     }
 
